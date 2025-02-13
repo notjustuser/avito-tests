e0bb22cc787c4d3f854c47bec7ba8fc6d7c1a349
notjustuser <soktoeva42@gmail.com>
2025-02-13 21:20:07 +0300

    Update MainPage.java

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -113,6 +112,7 @@ public class MainPage extends AbsBasePage {
 
     public MainPage search(String input) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(adListSelector));
         List<WebElement> oldListings = driver.findElements(adListSelector);
         WebElement field = waiter.getElement(searchSelector);
         field.click();
