d38fd7eba5f6f9ed4d055e9363f43ef3498c6eaf
notjustuser <soktoeva42@gmail.com>
2025-02-15 12:48:36 +0300

    Update EditAdTest.java

--- src/test/java/EditAdTest.java
+++ src/test/java/EditAdTest.java
@@ -1,18 +1,20 @@
-import components.popups.CreateAdPopup;
-import data.AdContent;
 import factory.WebDriverFactory;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import pages.MainPage;
+
+import static io.qameta.allure.Allure.step;
 
 public class EditAdTest {
 
     private WebDriver driver;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         driver = new WebDriverFactory().create();
     }
 
@@ -24,15 +26,21 @@ public class EditAdTest {
     }
 
     @Test
+    @DisplayName("Редактирование объявления")
+    @Description("Тест на проверку редактирования объявления и сохранение изменений")
     public void adEditing() {
         MainPage mainPage = new MainPage(driver);
-        mainPage.open("/");
-
-        mainPage.clickFirstAd()
-                .clickEditButton()
-                .input("name", "Носочный монстр")
-                .clickEditButton();
-
-        mainPage.checkName("Носочный монстр");
+        step("Открыть главную страницу", () -> {
+            mainPage.open("/");
+        });
+        step("Нажать на первое объявление и изменить его название", () -> {
+            mainPage.clickFirstAd()
+                    .clickEditButton()
+                    .input("name", "Носочный монстр")
+                    .clickEditButton();
+        });
+        step("Проверяем, изменилось ли название", () -> {
+            mainPage.checkName("Носочный монстр");
+        });
     }
 }
