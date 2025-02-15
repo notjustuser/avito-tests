50afacb2564cfa12f6fe8a1f7142db02dfb95e1a
notjustuser <soktoeva42@gmail.com>
2025-02-15 12:48:06 +0300

    Update CreateAdTest.java

--- src/test/java/CreateAdTest.java
+++ src/test/java/CreateAdTest.java
@@ -1,11 +1,15 @@
 import components.popups.CreateAdPopup;
 import data.AdContent;
 import factory.WebDriverFactory;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import pages.MainPage;
+
+import static io.qameta.allure.Allure.step;
 
 public class CreateAdTest {
 
@@ -24,29 +28,39 @@ public class CreateAdTest {
     }
 
     @Test
+    @DisplayName("Создание объявления")
+    @Description("Тест на проверку создания объявления")
     public void adCreation() {
         MainPage mainPage = new MainPage(driver);
         CreateAdPopup createAdPopup = new CreateAdPopup(driver);
-        mainPage.open("/");
+        step("Открыть главную страницу", () -> {
+            mainPage.open("/");
+        });
         createAdPopup
                 .popupShouldNotBeVisible();
-        mainPage
-                .clickCreateButton()
-                .popupShouldBeVisible();
+
+        step("Нажать на кнопку 'Создать' ", () -> {
+            mainPage
+                    .clickCreateButton()
+                    .popupShouldBeVisible();
+        });
         AdContent adContent = new AdContent(
                 "Букет цветов №4",
                 "1 200 ₽",
                 "Самый красивый букет цветов для вашей дамы",
                 "https://static-cdn4-2.vigbo.tech/u71583/83686/blog/5250868/4556660/68384931/1000-4b070b1999dcc7125db82baf0d71d017.jpg");
-        mainPage
-                .fullInput(adContent)
-                .clickSaveButton();
+        step("Заполнить поля и сохранить изменения", () -> {
+            mainPage
+                    .fullInput(adContent)
+                    .clickSaveButton();
+        });
         createAdPopup
                 .popupShouldNotBeVisible();
 
-        mainPage.search(adContent.getName())
-                .clickFirstAd()
-                .fullCheck(adContent);
+        step("Искать объявление по названию и проверить его содержание ", () -> {
+            mainPage.search(adContent.getName())
+                    .clickFirstAd()
+                    .fullCheck(adContent);
+        });
     }
 }
-
